26362753cc27b5cc34945e5231578fb074b037c9
Guminsung <babyms999@gmail.com>
2025-09-08 22:14:59 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int w, int num) {
+    int answer = 0;
+    int i = (num - 1) / w;            // 꺼내려는 상자의 세로 위치
+    int j;
+    if (i % 2 == 0)
+        j = (num - 1) % w;
+    else                              // 꺼내려는 상자의 가로 위치
+        j = (w - 1)  - ((num - 1) % w);
+    int x = (n - 1) / w;              // 마지막 상자의 세로 위치
+    if (x % 2 == 0)
+    {
+        int y = (n - 1) % w;
+        if (y >= j)
+            answer = x - i + 1;
+        else
+            answer = x - i;
+    }
+    else
+    {
+        int y = (w - 1) - ((n - 1) % w);
+        if (y <= j)
+            answer = x - i + 1;
+        else
+            answer = x - i;
+    }
+    return answer;
+}
