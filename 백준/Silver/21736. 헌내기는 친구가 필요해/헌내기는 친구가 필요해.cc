e9bf1248d1330542668665164e51a8cbc9eff5fa
Guminsung <babyms999@gmail.com>
2025-09-12 15:00:05 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 20 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void dfs(int x, int y);                           // dfs 탐색을 위해 함수 정의
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1}; // 델타 탐색을 위한 이동 배열
+string campus[600];                               // 캠퍼스 정보를 받기 위한 문자열 배열
+bool visited[600][600] = {false};                 // 방문 확인 배열
+int n, m, cnt = 0;                                // 만날 수 있는 사람 수 카운팅
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x, y;
+    bool check = false;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> campus[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (check)
+            break; // 도연이를 찾았으면 반복문 종료
+        for (int j = 0; j < m; j++)
+            if (campus[i][j] == 'I')
+            {
+                x = i, y = j; // 도연이의 위치 탐색
+                check = true;
+                break;
+            }
+    }
+    dfs(x, y);
+    if (cnt > 0)
+        cout << cnt;
+    else
+        cout << "TT";
+    return 0;
+}
+
+void dfs(int x, int y)
+{
+    visited[x][y] = true; // 방문 체크
+    if (campus[x][y] == 'P')
+        cnt++; // 사람 발견 시 카운트 증가
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+            if (!visited[nx][ny] && campus[nx][ny] != 'X')
+                dfs(nx, ny);
+    }
+}
