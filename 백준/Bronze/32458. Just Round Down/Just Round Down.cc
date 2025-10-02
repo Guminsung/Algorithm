1e1e59cdc64607bd045fbd81d0df7ccd78a7d914
Guminsung <babyms999@gmail.com>
2025-10-03 01:25:08 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    double num;
+    cin >> num;
+    cout << (int)num;
+    return 0;
+}
