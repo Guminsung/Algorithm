2e8fbd9761d33c67a27112f94f4c7d7ae9a85257
Guminsung <babyms999@gmail.com>
2025-10-10 12:53:00 +0900

    [Gold III] Title: 텀 프로젝트, Time: 416 ms, Memory: 3160 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -14,14 +14,13 @@ int main()
     {
         int ans = 0; // 정답 저장 변수
         cin >> n;
-        vector<vector<int>> std(n + 1); // 학생들의 관계 저장 배열
-        vector<int> deg(n + 1);         // 진입 차수 저장 배열
-        queue<int> q;                   // 진입차수 0인 노드 저장 큐
+        vector<int> std(n + 1); // 학생들의 관계 저장 배열
+        vector<int> deg(n + 1); // 진입 차수 저장 배열
+        queue<int> q;           // 진입차수 0인 노드 저장 큐
         for (int j = 1; j <= n; j++)
         {
-            cin >> num;
-            std[j].push_back(num); // 학생들이 선택한 번호 저장
-            deg[num]++;            // 진입차수 표시
+            cin >> std[j]; // 학생들이 선택한 번호 저장
+            deg[std[j]]++; // 진입차수 표시
         }
         for (int j = 1; j <= n; j++)
             if (deg[j] == 0)
@@ -31,12 +30,9 @@ int main()
             int p = q.front(); // 학생 번호 할당
             ans++;             // 정답 수 증가
             q.pop();
-            for (int j = 0; j < std[p].size(); j++)
-            {
-                deg[std[p][j]]--; // 해당 학생 관계 제거
-                if (deg[std[p][j]] == 0)
-                    q.push(std[p][j]); // 진입차수가 0이 된 학생 삽입
-            }
+            deg[std[p]]--; // 해당 학생 관계 제거
+            if (deg[std[p]] == 0)
+                q.push(std[p]); // 진입차수가 0이 된 학생 삽입
         }
         cout << ans << "\n";
     }
