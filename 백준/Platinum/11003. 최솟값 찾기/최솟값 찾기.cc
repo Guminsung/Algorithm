b77c1c9a616ff34731ff02f900a7accc631b9788
Guminsung <babyms999@gmail.com>
2025-11-17 23:09:41 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1160 ms, Memory: 43588 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,29 +1,23 @@
-#include <iostream>
-#include <queue>
-#include <utility>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, l, A;
-    cin >> n >> l;
-    // 최솟값과 번호 저장 우선순위 큐
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> A;
-        pq.push({A, i});
-        int idx = pq.top().second; // 큐에 저장된 수의 번호 할당
-        while (idx <= i - l)
-        {
-            pq.pop();
-            idx = pq.top().second; // 범위 밖 수 모두 큐에서 방출
-        }
-        int a = pq.top().first; // 최솟값 할당
-        cout << a << " ";
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, L;
+    cin >> N >> L;
+
+    deque<pair<int,int>> dq; // (값, 인덱스)
+    for (int i = 0; i < N; ++i) {
+        int a; cin >> a;
+
+        while (!dq.empty() && dq.back().first > a) dq.pop_back();
+        dq.emplace_back(a, i);
+
+        while (!dq.empty() && dq.front().second <= i - L) dq.pop_front();
+
+        cout << dq.front().first << ' ';
     }
     return 0;
 }
