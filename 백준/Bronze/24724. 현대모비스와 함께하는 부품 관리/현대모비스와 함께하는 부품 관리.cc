c912e3992755347ab5d9e2c1de80ea49394dbce7
Guminsung <babyms999@gmail.com>
2025-12-11 16:48:43 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -6,13 +6,10 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t, n, a, b, u, v;
+    int t;
     cin >> t;
     for (int i = 1; i <= t; i++)
     {
-        cin >> n >> a >> b;
-        while (n--)
-            cin >> u >> v;
         // 부품 관리 문구 출력
         cout << "Material Management " << i << "\n";
         // 프로그램 완료 문구 출력
